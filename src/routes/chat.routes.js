b6d77952de8bcef9862419ac150eb150ce2d092b
Gise Calderón <gisecalderon08@gmail.com>
2025-02-07 06:29:22 -0300

    commit...

---
+++ src/routes/chat.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const chatRouter = Router()
+
+chatRouter.get('/', (req,res) => {
+    res.render('templates/chat')
+})
+
+export default chatRouter
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { create } from 'express-handlebars';
 import { Server } from 'socket.io';
-import { path } from 'path';
+import  path  from 'path';
 import { __dirname } from './path.js'
 import productRouter from './routes/productos.routes.js';
 import cartRouter from './routes/carritos.routes.js';
 import multerRouter from './routes/imagenes.routes.js';
+import chatRouter from './routes/chat.routes.js';
 
 const app = express();
 const hbs = create()
@@ -20,12 +21,12 @@ app.use(express.urlencoded({extended:true}))
 app.engine('handlebars',hbs.engine)
 app.set('view engine', 'handlebars')
 
-app.use('/static', express.static(__dirname + '/public'))
-app.use('views', path.join(__dirname, 'views'))
-console.log(__dirname);
+app.use('views', path.join(__dirname, 'views'));
 
+app.use('/public', express.static(__dirname + '/public'))
 app.use('/api/productos', productRouter)
 app.use('/api/carts', cartRouter)
+app.use('/api/chat', chatRouter)
 app.use('/upload', multerRouter)
 const productos =[
     {nombre: "Remera S", marca: "GSC", precio: 16800, stock: 4, status: true},
@@ -33,15 +34,17 @@ const productos =[
     {nombre: "Remera J", marca: "MAPF", precio: 35000, stock: 9, status: false}
 ]
 app.get('/', (req,res) => {
-    res.render('productos',{productos})
+    res.render('/templates/productos',{productos: productos, js: 'productos.js', css: 'productos.css'})
 })
-
+let mensajes = []
 io.on('connection',(socket) =>{
     console.log('Usuario conectado: ', socket.id);
 
-    socket.on('mensaje', (data) =>{
+
+    socket.on('mensaje', (data) =>  {
         console.log('Mensaje recibido: ', data);
-        socket.emit('respuesta', 'Mensaje recibido correctamente: ', data)
+        mensajes.push(data)
+        socket.emit('respuesta', mensajes)
     })
 
     socket.on('disconnect', () => {
